8efb7d352eee2391deedd63f4665480a7dcaefff
swelzh <swelzh@gmail.com>
2019-01-08 01:36:22 +0800

    Initial commit

---
+++ Example/iOSViews/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSViews
+//
+//  Created by swelzh@gmail.com on 01/08/2019.
+//  Copyright (c) 2019 swelzh@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
